c5473dffc776a309ac80092d5d79da832805a10e
Josh <jgordon1401@gmail.com>
2021-12-14 22:50:21 -0500

    Fixed security issues

---
+++ README.md
@@ -0,0 +1,7 @@
+# **Sith Reddit Bot**
+## **Only a Sith deals in absolutes**
+
+### *By Josh Gordon*
+
+Responds to comments with certain variations of the term: 'absolute'
+
---
+++ botMain.py
@@ -0,0 +1,76 @@
+
+import praw
+import pdb
+import re
+import os
+import sys
+
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('password.ini')
+
+botPass = config.get('s1', 'pass')
+devPass = config.get('s1', 'devPass')
+
+numberOfTopPostsToCheck = 100
+phrasesToRecognize = {"absolute", "absolutes", "absolutely", "absolution"}
+subredditToSearch = 'relevantrealname'
+#'StarWars'
+#'AbsoluteUnits'
+#
+#'PrequelMemes'
+
+reddit = praw.Reddit(
+    client_id="tRclfC6esR4Mz3-5B4Axjg",
+    client_secret= devPass,
+    password= botPass,
+    user_agent="testscript by u/SithLord_Bot",
+    username="SithLord_Bot",
+)
+
+print(reddit.user.me())
+
+subreddit = reddit.subreddit(subredditToSearch)
+
+# Create a list
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+
+# Or load the list of posts we have replied to
+else:
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+
+
+for submission in subreddit.hot(limit=numberOfTopPostsToCheck):
+    #print(submission.title)
+    # Make sure you didn't already reply to this post
+    if submission.id not in posts_replied_to:
+            #Search through comments for keywords
+            
+            for currentComment in submission.comments:
+                # Not case sensitive
+                
+                for keyword in phrasesToRecognize:
+                    try:
+                        if re.search(keyword, currentComment.body, re.IGNORECASE):
+                            if submission.id not in posts_replied_to:
+                                # Reply
+                                if not currentComment.locked:
+                                    currentComment.reply("Only a Sith deals in absolutes.")
+                                    print("Bot replying to : ", submission.title)
+
+                                    # Store id in list
+                                    posts_replied_to.append(submission.id)
+                                    break
+                    except Exception:
+                        print("Wack")
+
+# Write updated list to file
+with open("posts_replied_to.txt", "w") as f:
+    for post_id in posts_replied_to:
+        f.write(post_id + "\n")
