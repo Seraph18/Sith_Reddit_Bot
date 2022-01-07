9cbed6ab2522c1a380ec0e0f4eb805c50b44c28a
Josh <jgordon1401@gmail.com>
2022-01-07 14:05:25 -0500

    Will now search multiple subreddits in one run and detect when too many comments have been posted and wait the appropriate amount of time before continuing.

--- botMain.py
+++ botMain.py
@@ -4,6 +4,7 @@ import pdb
 import re
 import os
 import sys
+import time
 
 from configparser import ConfigParser
 
@@ -15,10 +16,7 @@ devPass = config.get('s1', 'devPass')
 
 numberOfTopPostsToCheck = 200
 phrasesToRecognize = {"absolute", "absolutes", "absolutely", "absolution"}
-subredditToSearch = 'pics'
-# 'StarWars'
-# 'AbsoluteUnits'
-# 'PrequelMemes'
+subredditToSearchList = {'all', 'StarWars', 'AbsoluteUnits', 'PrequelMemes'}
 
 reddit = praw.Reddit(
     client_id="tRclfC6esR4Mz3-5B4Axjg",
@@ -28,85 +26,112 @@ reddit = praw.Reddit(
     username="SithLord_Bot",
 )
 
-print(reddit.user.me())
+print("Current bot = ", reddit.user.me())
 
-subreddit = reddit.subreddit(subredditToSearch)
+for subredditToSearch in subredditToSearchList:
 
-# Create a list
-if not os.path.isfile("posts_replied_to.txt"):
-    posts_replied_to = []
+    subreddit = reddit.subreddit(subredditToSearch)
 
-# Or load the list of posts we have replied to
-else:
-    with open("posts_replied_to.txt", "r") as f:
-        posts_replied_to = f.read()
-        posts_replied_to = posts_replied_to.split("\n")
-        posts_replied_to = list(filter(None, posts_replied_to))
+    # Create a list
+    if not os.path.isfile("posts_replied_to.txt"):
+        posts_replied_to = []
 
+    # Or load the list of posts we have replied to
+    else:
+        with open("posts_replied_to.txt", "r") as f:
+            posts_replied_to = f.read()
+            posts_replied_to = posts_replied_to.split("\n")
+            posts_replied_to = list(filter(None, posts_replied_to))
 
-numberPost = 0
-absoluteReplies = 0
-fatherReplies = 0
-# searchResultssubreddit.search()
+    numberPost = 0
+    absoluteReplies = 0
+    fatherReplies = 0
+    # searchResultssubreddit.search()
 
-for submission in subreddit.hot(limit=numberOfTopPostsToCheck):
-    # print(submission.title)
-    # Make sure you didn't already reply to this post
+    for submission in subreddit.hot(limit=numberOfTopPostsToCheck):
+        # print(submission.title)
+        # Make sure you didn't already reply to this post
 
-    numberPost = numberPost + 1
-    print("Checked ", numberPost)
-    #print("Post Name: ", submission.title)
+        numberPost = numberPost + 1
+        print("Checked ", numberPost)
+        #print("Post Name: ", submission.title)
 
-    if submission.id not in posts_replied_to:
-        # Search through comments for keywords
+        if submission.id not in posts_replied_to:
+            # Search through comments for keywords
 
-        for currentComment in submission.comments:
-            # Not case sensitive
+            for currentComment in submission.comments:
+                # Not case sensitive
 
-            for keyword in phrasesToRecognize:
-                try:
-                    if submission.id not in posts_replied_to:
+                for keyword in phrasesToRecognize:
+                    try:
+                        if submission.id not in posts_replied_to:
 
-                        if re.search(keyword, currentComment.body, re.IGNORECASE):
+                            if re.search(keyword, currentComment.body, re.IGNORECASE):
 
-                            # Reply
+                                # Reply
 
-                            currentComment.reply(
-                                "Only a Sith deals in absolutes.")
+                                currentComment.reply(
+                                    "Only a Sith deals in absolutes.")
 
-                            print(numberPost, ": Bot replying to : ",
-                                  submission.title)
+                                print(numberPost, ": Bot replying to : ",
+                                      submission.title)
 
-                            # Store id in list
-                            posts_replied_to.append(submission.id)
-                            absoluteReplies = absoluteReplies + 1
+                                # Store id in list
+                                posts_replied_to.append(submission.id)
+                                absoluteReplies = absoluteReplies + 1
+                                break
+
+                        else:
+                            # print("I already commented on this post.")
                             break
 
-                    else:
-                       # print("I already commented on this post.")
-                        break
+    # Handle errors for Absolute response
+
+                    except Exception as e:
+                        errorMessage = e
+                        print(e)
+
+                        # Handle too many comments
+                        if(errorMessage.contains("You're doing that too much. Try again in")):
+                            currentWord = ""
+                            minutesToWait = minutesToWait = int(
+                                re.search(r'\d+', str).group())
+                            secondsToWait = minutesToWait * 60
+                            print("Sleeping for ", minutesToWait,
+                                  " minutes, aka ", secondsToWait, " seconds.")
+                            time.sleep(secondsToWait)
+
+                try:
+                    if re.search("father", currentComment.body, re.IGNORECASE):
+                        if submission.id not in posts_replied_to:
+                            fatherReply = str(
+                                currentComment.author) + ", I am your father."
+                            currentComment.reply(fatherReply
+                                                 )
+
+                            posts_replied_to.append(submission.id)
+                            fatherReplies = fatherReplies + 1
+                            break
+
+    # Handle error for father response
 
                 except Exception as e:
-                    print(e)
+                    errorMessage = e
+                    print(errorMessage)
 
-            try:
-                if re.search("father", currentComment.body, re.IGNORECASE):
-                    if submission.id not in posts_replied_to:
-                        fatherReply = str(currentComment.author) + ", I am your father."
-                        currentComment.reply( fatherReply
-                            )
+                    if(errorMessage.contains("You're doing that too much. Try again in")):
+                        currentWord = ""
+                        minutesToWait = minutesToWait = int(
+                            re.search(r'\d+', str).group())
+                        secondsToWait = minutesToWait * 60
+                        print("Sleeping for ", minutesToWait,
+                              " minutes, aka ", secondsToWait, " seconds.")
+                        time.sleep(secondsToWait)
 
-                        posts_replied_to.append(submission.id)
-                        fatherReplies = fatherReplies + 1
-                        break
+    # Write updated list to file
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
 
-            except Exception as e:
-                print(e)
-
-# Write updated list to file
-with open("posts_replied_to.txt", "w") as f:
-    for post_id in posts_replied_to:
-        f.write(post_id + "\n")
-
-print("Absolute Sith Comments: ", absoluteReplies)
-print("Father Comments: ", fatherReplies)
+    print("Absolute Sith Comments: ", absoluteReplies)
+    print("Father Comments: ", fatherReplies)
